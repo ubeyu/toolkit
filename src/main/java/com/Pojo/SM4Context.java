b76c3fc83996a530de3299bfdb2f46c9f2905c9d
wanghy6 <wanghaoyang949@gmail.com>
2023-02-15 10:42:52 +0800

    增加SM4_ECB_PKCS7加解密工具

---
+++ src/main/java/com/Pojo/SM4Context.java
@@ -0,0 +1,12 @@
+package com.Pojo;
+
+public class SM4Context {
+
+    public int mode = 1;
+    public long[] sk = new long[32];
+    public boolean isPadding = true;
+
+    public SM4Context() {
+    }
+
+}
---
+++ src/main/java/com/Util/SM4Utils.java
@@ -0,0 +1,73 @@
+package com.Util;
+
+import com.Pojo.SM4;
+import com.Pojo.SM4Context;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SM4Utils {
+
+    public static final Pattern P_MATCH = Pattern.compile("\\s*|\t|\r|\n");
+
+    /**
+     * SM4_ECB_PKCS7加密
+     * 密文长度不固定，会随着被加密字符串长度的变化而变化
+     */
+    public static String encryptDatas(String plainText, String secretKey) {
+        try {
+            SM4Context ctx = new SM4Context();
+            ctx.isPadding = true;
+            ctx.mode = 1;
+            byte[] keyBytes = secretKey.getBytes();
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_enc(ctx, keyBytes);
+            byte[] encrypted = sm4.sm4_crypt_ecb(ctx, plainText.getBytes("UTF-8"));
+            String cipherText = (new BASE64Encoder()).encode(encrypted);
+            if (cipherText != null && cipherText.trim().length() > 0) {
+                Matcher m = P_MATCH.matcher(cipherText);
+                cipherText = m.replaceAll("");
+            }
+            return cipherText;
+        } catch (Exception var8) {
+            var8.printStackTrace();
+            return null;
+        }
+    }
+    /**
+     * SM4_ECB_PKCS7解密
+     */
+    public static String decryptDatas(String cipherText, String secretKey) {
+        try {
+            SM4Context ctx = new SM4Context();
+            ctx.isPadding = true;
+            ctx.mode = 0;
+            byte[] keyBytes = secretKey.getBytes();
+            SM4 sm4 = new SM4();
+            sm4.sm4_setkey_dec(ctx, keyBytes);
+            byte[] decrypted = sm4.sm4_crypt_ecb(ctx, (new BASE64Decoder()).decodeBuffer(cipherText));
+            return new String(decrypted, "UTF-8");
+        } catch (Exception var6) {
+            var6.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 加解密key
+        String secretKey = "";
+        // 加解密data
+        String data = "";
+
+        System.out.println("1) 传输数据:"+data);
+        //加密
+        String encrypte = encryptDatas(data, secretKey);
+        System.out.println("2) 加密后数据:"+encrypte);
+        //解密
+        String decryptDatas = decryptDatas(encrypte, secretKey);
+        System.out.println("3) 解密后数据："+decryptDatas);
+    }
+
+}
